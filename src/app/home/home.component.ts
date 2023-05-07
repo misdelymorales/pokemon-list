eb34bf471068b48cd7a49b088a5710ce3d41e754
Misdely Morales <mmoralbr@emeal.nttdata.com>
2023-05-06 22:05:53 -0400

    ADD: Pokemon API Consumption creation of service and component that gets data, shows pokemons and pages them

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../services/pokemon.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  
+  name: {};
+  
+  constructor(private pokemonService : PokemonService) { 
+    this.name = {};
+  }
 
+  ngOnInit(): void {
+    
+  }
+
+  search(){
+    this.pokemonService.getPokemons(this.name).subscribe(
+      (data : any) => {
+        console.log(data)
+        this.name = data;
+      }
+    )
+  }
 }
