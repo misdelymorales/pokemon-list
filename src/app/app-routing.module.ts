be5bf60e5bcb8cd779ddc82a9ea1005345b9c5c6
Misdely Morales <mmoralbr@emeal.nttdata.com>
2023-05-12 15:06:04 -0400

    DOCS: Changes are generated in the documentation Changes are generated in the project documentation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
 
 const routes: Routes = [
@@ -11,10 +10,6 @@ const routes: Routes = [
   {
     path: 'index',
     component: PokemonCardComponent,
-  },
-  {
-    path: 'home',
-    component: HomeComponent,
   },
 ];
 
